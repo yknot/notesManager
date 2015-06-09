bd2cc249b6f71a14863665e58a6214e6e7fcffd6
Andrew Yale <a.yale9@gmail.com>
2015-06-09 17:54:06 -0400

    initial commit

---
+++ note.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+# open config file
+
+# read in base folder
+# read in text editor
+
+# check for args
+
+# no args print usage
+
+
+# case statement args
+
+
+# -t returns all notes
+# print in tree format
+
+
+# -g <string> searches for all
+# return list of options
+
+
+# -m <string> makes a new note
+
+
+# -f <string> tree output of a subfolder
