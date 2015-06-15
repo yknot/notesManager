114cde80deef55b1d8c7919a24243fb409f5bd87
Andrew Yale <a.yale9@gmail.com>
2015-06-15 17:56:21 -0400

    add preliminary list and search

--- note.sh
+++ note.sh
@@ -1,28 +1,71 @@
 #!/bin/bash
 
 
-# open config file
-
+# open config files
 # read in base folder
+BASE=$(<base.config)
 # read in text editor
+EDITOR=$(<editor.config)
 
-# check for args
-
-# no args print usage
-
+# print usage function
+printUsage() {
+echo "Usage: note
+  list - list all notes
+  search <string> - searches all notes for the string
+"
+}
 
 # case statement args
+if [ "$1" = "" ]; then
+  if [[ "$BASE" = "" ]]; then
+    echo "Error with base.config file"
+  fi
+  if [[ "$EDITOR" = "" ]]; then
+    echo "Error with editor.config file"
+  fi
+
+  printUsage
+  exit
+fi
 
 
-# -t returns all notes
-# print in tree format
+case $1 in
+  # -t returns all notes
+  # print in tree format
+  list)
+    ls -R $BASE
+    exit
+  ;;
+
+  # -g <string> searches for all
+  # return list of options
+  search)
+    if [ "$2" = "" ]; then
+      echo "Need search string"
+      printUsage
+      exit
+    fi
+
+    # search file names
+    ls -R $BASE | grep "$2"
+
+    # search in files
+    grep "$2" $BASE -R
 
 
-# -g <string> searches for all
-# return list of options
+  ;;
+
+  # -n <string> makes a new note
 
 
-# -m <string> makes a new note
+  # -f <string> tree output of a subfolder
 
 
-# -f <string> tree output of a subfolder
+
+
+
+  *)
+    printUsage
+
+  ;;
+esac
