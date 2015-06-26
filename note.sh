7202befe02c0328b83489652d9c52552b9b89462
Andrew Yale <a.yale9@gmail.com>
2015-06-26 07:34:36 -0400

    added delete option

--- note.sh
+++ note.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-
-# open config files
-# read in base folder
-BASE=$(<base.config)
-# read in text editor
-EDITOR=$(<editor.config)
-
 # print usage function
 printUsage() {
 echo "Usage: note
@@ -14,6 +7,7 @@ echo "Usage: note
   -l - list all notes
   -s <string> - searches all notes for the string
   -f <string> -n <string> - create new note with path -f and name -n
+  -d <string> - delete note given relative path
 "
 }
 # standard list all function
@@ -24,6 +18,29 @@ listFiles() {
   # print in tree format
   tree -f --charset=ascii $BASE | fpp
 }
+# ask for confirmation
+ask() {
+
+  # ask for confirmation
+  read -p "$1 [y/n] " REPLY
+
+  # default to no
+  if [ -z "$REPLY" ]; then
+    REPLY="n"
+  fi
+
+  # 0 is true in bash
+  case "$REPLY" in
+    Y*|y*) return 0 ;;
+    N*|n*) return 1 ;;
+  esac
+}
+
+# open config files
+# read in base folder
+BASE=$(<base.config)
+# read in text editor
+EDITOR=$(<editor.config)
 
 # make sure editor and base are set
 if [[ "$BASE" = "" ]]; then
@@ -39,7 +56,7 @@ fi
 
 
 # loop through all flags and their args
-while getopts “hls:f:n:” OPTION
+while getopts “hls:f:n:d:” OPTION
 do
   case $OPTION in
 
@@ -79,11 +96,25 @@ do
       folder=$OPTARG
     ;;
 
-    # -n <string> makes a new note
+    # -n <string> set the filename for the new note
     n)
       note=$OPTARG
     ;;
 
+    # -d <string> delete note at path
+    d)
+    # create path
+     path=$BASE/$OPTARG
+     # ask to confirm delete
+     if ask "Do you want to delete $path?" ; then
+      rm $path
+       echo "$path was deleted"
+     else
+       echo "$path was not deleted"
+     fi
+
+     exit
+    ;;
   esac
 done
 
@@ -95,6 +126,7 @@ if [ "$folder" = "" ] && [ "$note" = "" ]; then
   listFiles
   exit
 
+# if make new file and both values are there
 elif [ "$folder" != "" ] && [ "$note" != "" ]; then
   # folder and note != ""
 
